43cdf4f60c136932b2c3573560b42735d9b3dccc
tabakazu <tkazz.bd1@gmail.com>
2021-03-23 23:54:52 +0900

    add slice trick sample for push, pop, reversing

--- slice-tricks-demo/example2.go
+++ slice-tricks-demo/example2.go
@@ -14,8 +14,21 @@ func main() {
 	a = append(a, b...)
 	fmt.Println(a) // [100 5 10000]
 
+	// Push
+	a = append(a, 200)
+	fmt.Println(a) // [100 5 10000 200]
+
+	// Pop
+	x, a := a[len(a)-1], a[:len(a)-1]
+	fmt.Println(x) // 200
+	fmt.Println(a) // [100 5 10000]
+
 	// Get Max Int From Slice
 	fmt.Println(getMaxIntFromSlice(a)) // 10000
+
+	// Reversing
+	reverseIntSlice(a)
+	fmt.Println(a) // [10000 5 100]
 }
 
 func getMaxIntFromSlice(slice []int) int {
@@ -26,4 +39,11 @@ func getMaxIntFromSlice(slice []int) int {
 		}
 	}
 	return max
+}
+
+func reverseIntSlice(slice []int) {
+	for i := len(slice)/2 - 1; i >= 0; i-- {
+		opp := len(slice) - 1 - i
+		slice[i], slice[opp] = slice[opp], slice[i]
+	}
 }
