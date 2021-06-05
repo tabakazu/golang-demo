b6db6189f1a2f29e984d20e66e5d95fa914a7597
tabakazu <tkazz.bd1@gmail.com>
2021-06-05 22:31:50 +0900

    add example that using sync.WaitGroup for sync func

--- goroutine-demo/goroutine-webapi-demo/main.go
+++ goroutine-demo/goroutine-webapi-demo/main.go
@@ -1,10 +1,15 @@
+// Usage
+//   $ go run main.go
+//   $ curl http://localhost:8080/goroutine
+//   $ curl http://localhost:8080/not_goroutine
+
 package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -25,8 +30,30 @@ type Response struct {
 func GoroutineHandler(w http.ResponseWriter, r *http.Request) {
 	defer log.Println("finish GoroutineHandler.")
 	log.Printf("%v - %v", r.Method, r.URL)
-	json.NewEncoder(w).Encode(&Response{"後続処理は非同期で処理"})
 
+	// 時間のかかる事前作業を並行実行して同期
+	res := func() *Response {
+		ret := &Response{"後続処理は非同期で処理"}
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			SleepAndPrint("事前処理 1")
+			ret.Name = ret.Name + "!"
+		}()
+		go func() {
+			defer wg.Done()
+			SleepAndPrint("事前処理 2")
+			ret.Name = ret.Name + "!!"
+		}()
+
+		wg.Wait()
+		return ret
+	}()
+	json.NewEncoder(w).Encode(res)
+
+	// 遅延処理で問題ない時間のかかる後続作業を非同期処理
 	go func() {
 		SleepAndPrint("遅延処理 1") // 時間がかかる処理があると想定
 	}()
@@ -39,12 +66,24 @@ func GoroutineHandler(w http.ResponseWriter, r *http.Request) {
 func NotGoroutineHandler(w http.ResponseWriter, r *http.Request) {
 	defer log.Println("finish NotGoroutineHandler.")
 	log.Printf("%v - %v", r.Method, r.URL)
-	json.NewEncoder(w).Encode(&Response{"すべて同期して処理"})
+
+	res := func() *Response {
+		ret := &Response{"すべて同期して処理"}
+
+		SleepAndPrint("事前処理 1")
+		ret.Name = ret.Name + "!"
+		SleepAndPrint("事前処理 2")
+		ret.Name = ret.Name + "!!"
+
+		return ret
+	}()
+	json.NewEncoder(w).Encode(res)
+
 	SleepAndPrint("同期処理 1") // 時間がかかる処理があると想定
 	SleepAndPrint("同期処理 2") // 時間がかかる処理があると想定
 }
 
 func SleepAndPrint(s string) {
 	time.Sleep(1 * time.Second)
-	fmt.Println(s)
+	log.Println(s)
 }
