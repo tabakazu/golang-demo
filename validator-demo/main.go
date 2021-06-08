ac2f8e13bc8eaf8475dde09408d02ffc73d64810
tabakazu <tkazz.bd1@gmail.com>
2021-06-09 00:35:10 +0900

    add example for custom validation

--- validator-demo/main.go
+++ validator-demo/main.go
@@ -6,20 +6,39 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+type User struct {
+	Email string `validate:"required,email,unique"`
+}
+
 func main() {
-	type S struct {
-		Email string `validate:"required,email"`
-	}
+	u := &User{Email: "example__example.com"}
+	validateUser(u)
 
+	u2 := &User{Email: ""}
+	validateUser(u2)
+
+	u3 := &User{Email: "unique@example.com"}
+	validateUser(u3)
+}
+
+func validateUser(u *User) {
 	validate := validator.New()
+	validate.RegisterValidation("unique", ValidateUserEmailUnique)
 
-	s1 := &S{Email: "example@example.com"}
-	if err := validate.Struct(s1); err != nil {
-		fmt.Printf("s1 error : %v\n", err)
+	if err := validate.Struct(u); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func ValidateUserEmailUnique(fl validator.FieldLevel) bool {
+	// db 検索想定
+	findByEmail := func(email string) *User {
+		if email == "unique@example.com" {
+			return &User{"unique@example.com"}
+		}
+		return nil
 	}
 
-	s2 := &S{Email: "example_example.com"}
-	if err := validate.Struct(s2); err != nil {
-		fmt.Printf("s2 error : %v\n", err)
-	}
+	u := findByEmail(fl.Field().String())
+	return u == nil
 }
