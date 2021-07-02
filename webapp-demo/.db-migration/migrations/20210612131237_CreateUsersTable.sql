b0ddfc929b0ed348d6cfbd763042ac4efb939985
tabakazu <tkazz.bd1@gmail.com>
2021-07-02 23:30:01 +0900

    rename dir of db schema management

--- webapp-demo/.db-migration/migrations/20210612131237_CreateUsersTable.sql
+++ webapp-demo/.db-migration/migrations/20210612131237_CreateUsersTable.sql
@@ -3,13 +3,14 @@
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '' COMMENT 'ユーザネーム',
   `family_name` varchar(128) NOT NULL DEFAULT '' COMMENT '姓',
   `given_name` varchar(128) NOT NULL DEFAULT '' COMMENT '名',
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_users_on_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
---
+++ webapp-demo/README.md
@@ -0,0 +1,12 @@
+# WebApp Demo
+
+```bash
+# build all images
+$ docker-compose build
+
+# login web server
+$ docker-compose run --rm go_app bash
+
+# start web server
+$ docker-compose up go_app
+```
